bc4017e488557cae5a0636c678a656078198b95a
JohnBensen1000 <jbensen@purdue.edu>
2021-07-05 10:53:25 -0400

    removed dependancies that weren't needed

--- lib/sections/camera/camera.dart
+++ lib/sections/camera/camera.dart
@@ -3,8 +3,6 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:image/image.dart' as image;
 
